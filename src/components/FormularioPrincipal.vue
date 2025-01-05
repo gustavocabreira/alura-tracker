5c5b144efa971538432854b4548eb04126fb7936
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-01-05 20:43:08 -0300

    feat: add start and stop button functions

--- src/components/FormularioPrincipal.vue
+++ src/components/FormularioPrincipal.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="box">
-        <div class="columns">
-            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
-            </div>
-            <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <section>
-                        <strong>00:00:00</strong>
-                    </section>
-                    <div class="is-flex is-align-items-center">
-                        <button class="button">
-                            <span class="icon">
-                                <i class="fas fa-play"></i>
-                            </span>
-                            <span>play</span>
-                        </button>
-                        <button class="button">
-                            <span class="icon">
-                                <i class="fas fa-stop"></i>
-                            </span>
-                            <span>stop</span>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="box">
+		<div class="columns">
+			<div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+				<input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+			</div>
+			<div class="column">
+				<div class="is-flex is-align-items-center is-justify-content-space-between">
+					<section>
+						<strong>{{ elapsedTime }}</strong>
+					</section>
+					<div class="is-flex is-align-items-center">
+						<button class="button" @click="start">
+							<span class="icon">
+								<i class="fas fa-play"></i>
+							</span>
+							<span>play</span>
+						</button>
+						<button class="button" @click="stop">
+							<span class="icon">
+								<i class="fas fa-stop"></i>
+							</span>
+							<span>stop</span>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -34,7 +34,29 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'FormularioPrincipal',
+	name: 'FormularioPrincipal',
+	data() {
+		return {
+			timeInSeconds: 0,
+			timer: 0,
+		}
+	},
+	computed: {
+		elapsedTime(): string {
+			return new Date(this.timeInSeconds * 1000).toISOString().substr(11, 8)
+		}
+	},
+	methods: {
+		start() {
+			this.timer = setInterval(() => {
+				this.timeInSeconds += 1;
+			}, 1000)
+		},
+		stop() {
+			this.timeInSeconds = 0;
+			clearInterval(this.timer)
+		},
+	}
 });
 
 </script>
