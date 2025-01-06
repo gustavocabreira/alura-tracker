09ced7717844e1bf3e3aefa160eaec4d1825527a
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-01-05 21:12:43 -0300

    feat: blocking buttons when starting the timer

--- src/components/TimerComponent.vue
+++ src/components/TimerComponent.vue
@@ -3,13 +3,13 @@
     <TimerDisplay :timeInSeconds="timeInSeconds" />
 
     <div class="is-flex is-align-items-center">
-      <button class="button" @click="start">
+      <button class="button" @click="start" :disabled="isTimerRunning">
         <span class="icon">
           <i class="fas fa-play"></i>
         </span>
         <span>play</span>
       </button>
-      <button class="button" @click="stop">
+      <button class="button" @click="stop" :disabled="!isTimerRunning">
         <span class="icon">
           <i class="fas fa-stop"></i>
         </span>
@@ -32,18 +32,24 @@ export default defineComponent({
     return {
       timeInSeconds: 0,
       timer: 0,
+      isTimerRunning: false,
     }
   },
   methods: {
     start() {
+      this.toggleTimer();
       this.timer = setInterval(() => {
         this.timeInSeconds += 1;
       }, 1000)
     },
     stop() {
+      this.toggleTimer();
       this.timeInSeconds = 0;
       clearInterval(this.timer)
     },
+    toggleTimer() {
+      this.isTimerRunning = !this.isTimerRunning;
+    }
   }
 });
 </script>
