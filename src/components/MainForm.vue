4df0a9e6bd8592cdc8084dad0b00992843f11e8b
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-01-05 21:10:37 -0300

    refactor: refactoring the MainForm to a new component

--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -5,25 +5,7 @@
 				<input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
 			</div>
 			<div class="column">
-				<div class="is-flex is-align-items-center is-justify-content-space-between">
-
-					<TimerDisplay :timeInSeconds="timeInSeconds" />
-					
-					<div class="is-flex is-align-items-center">
-						<button class="button" @click="start">
-							<span class="icon">
-								<i class="fas fa-play"></i>
-							</span>
-							<span>play</span>
-						</button>
-						<button class="button" @click="stop">
-							<span class="icon">
-								<i class="fas fa-stop"></i>
-							</span>
-							<span>stop</span>
-						</button>
-					</div>
-				</div>
+				<TimerComponent />
 			</div>
 		</div>
 	</div>
@@ -32,30 +14,13 @@
 <script lang="ts">
 
 import { defineComponent } from 'vue';
-import TimerDisplay from './TimerDisplay.vue';
+import TimerComponent from './TimerComponent.vue';
 
 export default defineComponent({
 	name: 'MainForm',
 	components: {
-		TimerDisplay,
+		TimerComponent,
 	},
-	data() {
-		return {
-			timeInSeconds: 0,
-			timer: 0,
-		}
-	},
-	methods: {
-		start() {
-			this.timer = setInterval(() => {
-				this.timeInSeconds += 1;
-			}, 1000)
-		},
-		stop() {
-			this.timeInSeconds = 0;
-			clearInterval(this.timer)
-		},
-	}
 });
 
 </script>
---
+++ src/components/TimerComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <TimerDisplay :timeInSeconds="timeInSeconds" />
+
+    <div class="is-flex is-align-items-center">
+      <button class="button" @click="start">
+        <span class="icon">
+          <i class="fas fa-play"></i>
+        </span>
+        <span>play</span>
+      </button>
+      <button class="button" @click="stop">
+        <span class="icon">
+          <i class="fas fa-stop"></i>
+        </span>
+        <span>stop</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TimerDisplay from './TimerDisplay.vue';
+
+export default defineComponent({
+  name: 'TimerComponent',
+  components: {
+    TimerDisplay,
+  },
+  data() {
+    return {
+      timeInSeconds: 0,
+      timer: 0,
+    }
+  },
+  methods: {
+    start() {
+      this.timer = setInterval(() => {
+        this.timeInSeconds += 1;
+      }, 1000)
+    },
+    stop() {
+      this.timeInSeconds = 0;
+      clearInterval(this.timer)
+    },
+  }
+});
+</script>
